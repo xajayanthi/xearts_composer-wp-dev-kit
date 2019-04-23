646788aee93549fe4bd6622f283fc8276066566e
xearts <xearts@xeartsnoMac-mini.local>
2019-04-23 13:42:38 +0900

    updated the script to use YML  processor and updated path variables

--- bin/config.sh
+++ bin/config.sh
@@ -3,36 +3,41 @@
 set -eu;
 
 WP_CLI=$(cd $(dirname $0);cd ../;pwd)/vendor/bin/wp
-JQ=jq
+YQ=yq
 
 ## Get config.
 ROOT=$(cd $(dirname $0);cd ../;pwd)
-DOC_ROOT=$ROOT
-WP_PATH=$DOC_ROOT/wp
 CONFIG_PATH=$ROOT/.env.json
 
+PUBLIC_DIR="/public_html"
+WP_DIR="/wp"
+WP_CONTENT_DIR="/wp-content"
+
+DOC_ROOT=$ROOT$PUBLIC_DIR
+WP_PATH=$DOC_ROOT$WP_DIR
+echo $WP_PATH
 if [ -f "$CONFIG_PATH" ]; then
-    DB_USER=$(cat $CONFIG_PATH | $JQ -r ".mysql.username")
-    DB_PASS=$(cat $CONFIG_PATH | $JQ -r ".mysql.password")
-    DB_NAME=$(cat $CONFIG_PATH | $JQ -r ".mysql.database")
-    DB_HOST=$(cat $CONFIG_PATH | $JQ -r ".mysql.host")
+    DB_USER=$($YQ r $CONFIG_PATH "mysql.username")
+    DB_PASS=$($YQ r $CONFIG_PATH  "mysql.password")
+    DB_NAME=$($YQ r $CONFIG_PATH  "mysql.database")
+    DB_HOST=$($YQ r $CONFIG_PATH  "mysql.host")
 
-    PORT=$(cat $CONFIG_PATH | $JQ -r ".server.port")
-    HOST=$(cat $CONFIG_PATH | $JQ -r ".server.host")
+    URL=$($YQ r $CONFIG_PATH  "server.url")
 
-    WP_TITLE=$(cat $CONFIG_PATH | $JQ -r ".wp.title")
-    WP_DESCRIPTION=$(cat $CONFIG_PATH | $JQ -r ".wp.description")
-    WP_LANG=$(cat $CONFIG_PATH | $JQ -r ".wp.lang")
-    WP_GMT_OFFSET=$(cat $CONFIG_PATH | $JQ -r ".wp.gmt_offset")
-    WP_REWRITE_STRUCTURE=$(cat $CONFIG_PATH | $JQ -r ".wp.rewrute_structure")
 
-    WP_ADMIN_USER=$(cat $CONFIG_PATH | $JQ -r ".wp.admin.user")
-    WP_ADMIN_PASSWORD=$(cat $CONFIG_PATH | $JQ -r ".wp.admin.password")
-    WP_ADMIN_EMAIL=$(cat $CONFIG_PATH | $JQ -r ".wp.admin.email")
+    WP_TITLE=$($YQ r $CONFIG_PATH  "wp.title")
+    WP_DESCRIPTION=$($YQ r $CONFIG_PATH  "wp.description")
+    WP_LANG=$($YQ r $CONFIG_PATH  "wp.lang")
+    WP_GMT_OFFSET=$($YQ r $CONFIG_PATH  "wp.gmt_offset")
+    WP_REWRITE_STRUCTURE=$($YQ r $CONFIG_PATH "wp.rewrute_structure")
 
-    WP_THEME=$(cat $CONFIG_PATH | $JQ -r ".wp.theme")
+    WP_ADMIN_USER=$($YQ r $CONFIG_PATH  "wp.admin.user")
+    WP_ADMIN_PASSWORD=$($YQ r $CONFIG_PATH  "wp.admin.password")
+    WP_ADMIN_EMAIL=$($YQ r $CONFIG_PATH  "wp.admin.email")
 
-    THEME_UNIT_TEST_URI=$(cat $CONFIG_PATH | $JQ -r ".theme_unit_test_uri")
+    WP_THEME=$($YQ r $CONFIG_PATH  "wp.theme")
+
+    THEME_UNIT_TEST_URI=$($YQ r $CONFIG_PATH  "theme_unit_test_uri")
 
 else
     echo ".env.json is NOT a file."
