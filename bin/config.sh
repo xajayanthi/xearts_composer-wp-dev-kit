1c3cb3fcbde6ff9686b9eedb85fc9662fe17d872
xearts <xearts@xeartsnoMac-mini.local>
2019-04-25 12:47:41 +0900

    Fixed directory structure values

--- bin/config.sh
+++ bin/config.sh
@@ -9,21 +9,20 @@ YQ=yq
 ROOT=$(cd $(dirname $0);cd ../;pwd)
 CONFIG_PATH=$ROOT/.env.json
 
-PUBLIC_DIR="/public_html"
-WP_DIR="/wp"
-WP_CONTENT_DIR="/wp-content"
-
-DOC_ROOT=$ROOT$PUBLIC_DIR
-WP_PATH=$DOC_ROOT$WP_DIR
-echo $WP_PATH
 if [ -f "$CONFIG_PATH" ]; then
+    PUBLIC_DIR=$($YQ r $CONFIG_PATH "dir_structure.public_dir")
+    WP_DIR=$($YQ r $CONFIG_PATH "dir_structure.wp_dir")
+    WP_CONTENT_DIR=$($YQ r $CONFIG_PATH "dir_structure.wp_content_dir")
+    DOC_ROOT=$ROOT$PUBLIC_DIR
+
     DB_USER=$($YQ r $CONFIG_PATH "mysql.username")
     DB_PASS=$($YQ r $CONFIG_PATH  "mysql.password")
     DB_NAME=$($YQ r $CONFIG_PATH  "mysql.database")
     DB_HOST=$($YQ r $CONFIG_PATH  "mysql.host")
 
     URL=$($YQ r $CONFIG_PATH  "server.url")
-
+    HOST=$($YQ r $CONFIG_PATH  "server.host")
+    PORT=$($YQ r $CONFIG_PATH  "server.port")
 
     WP_TITLE=$($YQ r $CONFIG_PATH  "wp.title")
     WP_DESCRIPTION=$($YQ r $CONFIG_PATH  "wp.description")
@@ -38,7 +37,6 @@ if [ -f "$CONFIG_PATH" ]; then
     WP_THEME=$($YQ r $CONFIG_PATH  "wp.theme")
 
     THEME_UNIT_TEST_URI=$($YQ r $CONFIG_PATH  "theme_unit_test_uri")
-
 else
     echo ".env.json is NOT a file."
     exit 0
